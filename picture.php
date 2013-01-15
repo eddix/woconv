9043afe56ad6c05f2fe1d4185d5f275abae19fb2
eddix <elitecharm@gmail.com>
2013-01-15 14:36:40 +0800

    add picture.php

---
+++ picture.php
@@ -0,0 +1,34 @@
+<?php
+/* video.php
+ *
+ * Retrieving video from gridfs base on mongodb
+ *
+ * Author: chenxin <chenxin@smapp.hk>
+ * Date: 2012-12-16
+ *
+ * */
+
+$filename = $_GET['f'] or die("Cannot get filename");
+
+$mongo = new MongoClient();
+$db = $mongo->pictures;
+$grid = $db->getGridFS();
+
+$video = $grid->findOne($filename);
+if ($video == NULL) {
+    die("Picture is not exists");
+}
+
+$length = $video->getSize();
+$stream = $video->getResource();
+
+header("Content-type: image/jpeg");
+header("Content-Length: $length");
+
+while (!feof($stream) && ($p = ftell($stream)) <= $end) {
+    echo fread($stream, 8192);
+    flush();
+}
+
+fclose($stream);
+?>
